3b79708b21e925939b87c184d6b1a001ea8abd7b
Leo Paillier <leo.paillier@gmail.com>
2019-02-14 15:46:12 +0100

    Displays progress during upload
    - Computes the complete list of files to upload before starting it.
    - Displays progress with tqdm bar during upload.
    - Doesn't modify json format.

--- deepocli/cmds/feedback.py
+++ deepocli/cmds/feedback.py
@@ -1,5 +1,7 @@
-import sys
 import os
+import sys
+import json
+import logging
 from .studio_helpers.http_helper import HTTPHelper
 from .studio_helpers.image import Image
 from .studio_helpers.task import Task
@@ -7,12 +9,11 @@ from .studio_helpers.task import Task
 ###############################################################################
 
 API_HOST = os.getenv('STUDIO_URL', 'https://studio.deepomatic.com/api/')
-
+SUPPORTED_IMG_FORMAT = ['bmp', 'jpeg', 'jpg', 'jpe', 'png']
 
 ###############################################################################
 
 class Client(object):
-
     def __init__(self, token=None, verify_ssl=True, check_query_parameters=True, host=None, user_agent_suffix='', pool_maxsize=20):
         if host is None:
             host = API_HOST
@@ -21,8 +22,55 @@ class Client(object):
         self.image = Image(self.http_helper)
         self.task = Task(self.http_helper)
 
+
+def get_all_files_with_ext(path, supported_ext, recursive=True):
+    """Scans path to find all supported images."""
+    images = []
+    if os.path.isfile(path):
+        if path.split('.')[-1].lower() in supported_ext:
+            images.append(path)
+    elif os.path.isdir(path):
+        if recursive:
+            for root, dirs, files in os.walk(path, topdown=False):
+                for name in files:
+                    if name.split('.')[-1].lower() in supported_ext:
+                        images.append(os.path.join(root, name))
+        else:
+            for file in os.listdir(path):
+                file_path = os.path.join(path, file)
+                if os.path.isfile(file_path) and file_path.split('.')[-1].lower() in supported_ext:
+                    images.append(file_path)
+    else:
+        raise RuntimeError("The path {} is neither a supported file {} nor a directory".format(path, supported_ext))
+
+    return images
+
+def get_all_files(paths, find_json=False, recursive=True):
+    """Retrieves all files from paths, either images or json if specified."""
+    # Make sure path is a list
+    paths = [paths] if not isinstance(paths, list) else paths
+
+    # Go through all paths and find corresponding files
+    file_ext = 'json' if find_json else SUPPORTED_IMG_FORMAT
+    files = []
+    for path in paths:
+        files += get_all_files_with_ext(path, file_ext, recursive)
+
+    return files
+
 def main(args):
+    # Initialize deepomatic client
     clt = Client()
-    for path in args.get('path', []):
-        clt.image.post_images(args.get('dataset_name', ''), path, args.get('org_slug', ''), args.get('recursive', False), args.get('json_file', False))
-    print("Done")
+
+    # Retrieve arguments
+    dataset_name = args.get('dataset')
+    org_slug = args.get('organization')
+    paths = args.get('path', [])
+    json_file = args.get('json_file', False)
+    recursive = args.get('recursive', False)
+
+    # Scan to find all files
+    files = get_all_files(paths=paths, find_json=json_file, recursive=recursive)
+
+    # Start uploading
+    clt.image.post_images(files=files, dataset_name=dataset_name, org_slug=org_slug, is_json=json_file)
--- deepocli/cmds/studio_helpers/image.py
+++ deepocli/cmds/studio_helpers/image.py
@@ -2,41 +2,35 @@
 import os
 import sys
 import json
-import logging
 import uuid
+import time
+import signal
 import threading
-import sys
-
+from tqdm import tqdm
+from .task import Task
 if sys.version_info >= (3,0):
     import queue as Queue
 else:
     import Queue
-from .task import Task
-import logging
-import time
-import signal
 
-supported_formats = ['bmp', 'jpeg', 'jpg', 'jpe', 'png']
-
+# Define thread parameters
+THREAD_NUMBER = 5
 q = Queue.Queue()
 count = 0
 run = True
 lock = threading.Lock()
 
-
 def handler(signum, frame):
-    global run, q
+    global run, q, pbar
     run = False
-    print("Stopping...")
+    pbar.close()
+    print("Stopping upload...")
     while not q.empty():
         q.get()
         q.task_done()
 
-
 def worker(self):
-    global count
-    global run
-    global q
+    global count, run, q, pbar
     while run:
         try:
             url, data, file = q.get(timeout=2)
@@ -44,53 +38,15 @@ def worker(self):
                 with open(file, 'rb') as fd:
                     rq = self._helper.post(url, data={"meta": data}, content_type='multipart/form', files={"file": fd})
                 self._task.retrieve(rq['task_id'])
-            except RuntimeError:
-                logging.error('Annotation format for image named {} is incorrect'.format(file))
+            except RuntimeError as e:
+                tqdm.write('Annotation format for image named {} is incorrect'.format(file))
+            pbar.update(1)
             q.task_done()
             lock.acquire()
             count += 1
             lock.release()
-            if count % 10 == 0:
-                logging.info('{} files uploaded'.format(count))
         except Queue.Empty:
             pass
-
-def get_files(path, recursive=True):
-    if os.path.isfile(path):
-        if path.split('.')[-1].lower() in supported_formats:
-            yield path
-    elif os.path.isdir(path):
-        if recursive:
-            for root, dirs, files in os.walk(path, topdown=False):
-                for name in files:
-                    if name.split('.')[-1].lower() in supported_formats:
-                        yield os.path.join(root, name)
-        else:
-            for file in os.listdir(path):
-                file_path = os.path.join(path, file)
-                if os.path.isfile(file_path) and file_path.split('.')[-1].lower() in supported_formats:
-                    yield  file_path
-    else:
-        raise RuntimeError("The path {} is neither a image file nor a directory".format(path))
-
-def get_json(path, recursive=True):
-    if os.path.isfile(path):
-        if path.split('.')[-1].lower() == 'json':
-            yield path
-    elif os.path.isdir(path):
-        if recursive:
-            for root, dirs, files in os.walk(path, topdown=False):
-                for name in files:
-                    if name.split('.')[-1].lower() == 'json':
-                        yield os.path.join(root, name)
-        else:
-            for file in os.listdir(path):
-                file_path = os.path.join(path, file)
-                if os.path.isfile(file_path) and file_path.split('.')[-1].lower() == 'json':
-                    yield  file_path
-    else:
-        raise RuntimeError("The path {} is neither a json file nor a directory".format(path))
-
 
 class Image(object):
     def __init__(self, helper, task=None):
@@ -100,62 +56,81 @@ class Image(object):
         self._task = task
 
 
-    def post_images(self, dataset_name, path, org_slug, recursive=False, json_file=False):
-        global run
+    def post_images(self, dataset_name, files, org_slug, is_json=False):
+        global run, pbar
         try:
             ret = self._helper.get('datasets/' + dataset_name + '/')
         except RuntimeError as err:
             raise RuntimeError("Can't find the dataset {}".format(dataset_name))
         commit_pk = ret['commits'][0]['uuid']
 
+        # Initialize threads
         threads = []
-        for i in range(5):
+        for i in range(THREAD_NUMBER):
             t = threading.Thread(target=worker, args=(self, ))
             t.daemon = True
             t.start()
             threads.append(t)
-
         signal.signal(signal.SIGINT, handler)
 
-        if not isinstance(path, list):
-            path = [path]
-        for elem in path:
-            if not json_file:
-                for file in get_files(elem, recursive):
-                    tmp_name = uuid.uuid4().hex
-                    q.put(('v1-beta/datasets/{}/commits/{}/images/'.format(dataset_name, commit_pk), json.dumps({'location': tmp_name}), file))
+        # Build the queue
+        total_images = 0
+        for file in files:
+            # If it's an image, add it to the queue
+            if file.split('.')[-1].lower() != 'json':
+                tmp_name = uuid.uuid4().hex
+                q.put(('v1-beta/datasets/{}/commits/{}/images/'.format(dataset_name, commit_pk), json.dumps({'location': tmp_name}), file))
+                total_images += 1
+            # If it's a json, deal with it accordingly
             else:
-                for file in get_json(elem, recursive):
-                    try:
-                        with open(file, 'rb') as fd:
-                            json_objects = json.load(fd)
-                    except ValueError as err:
-                        logging.debug(err)
-                        logging.error("Can't read file {}, skipping...".format(file))
-                        continue
-                    if isinstance(json_objects, dict):
-                        if 'location' not in json_objects:
-                            for ext in ('bmp', 'jpeg', 'jpg', 'jpe', 'png'):
-                                tmp_path = os.path.join(os.path.dirname(file), os.path.basename(file).replace('json', ext))
-                                if os.path.isfile(tmp_path):
-                                    json_objects['location'] = tmp_path
-                                    json_objects = [json_objects]
-                                    break
-                            else:
-                                logging.error("Can't find an image named {}".format(os.path.basename(file)))
-                                continue
-                    for i, json_object in enumerate(json_objects):
-                        image_path = os.path.join(os.path.dirname(file), json_object['location'])
-                        if not os.path.isfile(image_path):
-                            logging.error("Can't find an image named {}".format(json_object['location']))
+                # Verify json validity
+                try:
+                    with open(file, 'rb') as fd:
+                        json_objects = json.load(fd)
+                except ValueError as err:
+                    tqdm.write(err)
+                    tqdm.write("Can't read file {}, skipping...".format(file))
+                    continue
+
+                if isinstance(json_objects, dict):
+                    if 'location' not in json_objects:
+                        for ext in ('bmp', 'jpeg', 'jpg', 'jpe', 'png'):
+                            tmp_path = os.path.join(os.path.dirname(file), os.path.basename(file).replace('json', ext))
+                            if os.path.isfile(tmp_path):
+                                json_objects['location'] = tmp_path
+                                json_objects = [json_objects]
+                                break
+                        else:
+                            tqdm.write("Can't find an image for json {}".format(os.path.basename(file)))
                             continue
-                        image_key = uuid.uuid4().hex
-                        json_object['location'] = image_key
-                        q.put(('v1-beta/datasets/{}/commits/{}/images/'.format(dataset_name, commit_pk), json.dumps(json_object), image_path))
+
+                for i, json_object in enumerate(json_objects):
+                    image_path = os.path.join(os.path.dirname(file), json_object['location'])
+                    if not os.path.isfile(image_path):
+                        tqdm.write("Can't find an image named {}".format(json_object['location']))
+                        continue
+                    image_key = uuid.uuid4().hex
+                    json_object['location'] = image_key
+                    q.put(('v1-beta/datasets/{}/commits/{}/images/'.format(dataset_name, commit_pk), json.dumps(json_object), image_path))
+                    total_images += 1
+
+        # Initialize progressbar
+        print("Uploading images...")
+        pbar = tqdm(total=total_images)
+
+        # Process all files
         while not q.empty() and run:
             time.sleep(2)
         run = False
+
+        # Close properly
         q.join()
         for t in threads:
             t.join()
+        pbar.close()
+        if count == total_images:
+            print("All {} images have been uploaded.".format(count))
+        else:
+            print("{} images out of {} have been uploaded.".format(count, total_images))
+
         return True
