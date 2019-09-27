8222c4fc67abe7abb649c050a8f06198e47bbdd8
Hugo Maingonnat <hugo@deepomatic.com>
2019-09-27 16:03:33 +0200

    Fix wait condition (#113)
    The first pool ,which had no input queue, whenever idle was asked to stop by the MainLoop.
    The fact that it was idle doesn't mean it was done processing the stream. We thus wait for it to stop by itself.

--- deepomatic/cli/thread_base.py
+++ deepomatic/cli/thread_base.py
@@ -86,6 +86,7 @@ class ThreadBase(object):
         self.exit_event = exit_event
         self.stop_asked = False
         self.name = name or self.__class__.__name__
+        # Allow to not stop when an item is still being processed
         self.processing_item_lock = Lock()
         self.current_messages = current_messages
         self.alive = False
@@ -99,9 +100,22 @@ class ThreadBase(object):
         return True
 
     def stop(self):
+        # Can be called from the same thread or from another
         self.stop_asked = True
 
-    def stop_when_empty(self):
+    def wait_until_nothing_to_process(self):
+        # Must be called externally (from another thread)
+
+        if self.input_queue is None:
+            # When ThreadBase has no input queue
+            # Either the ThreadBase stop by itself by calling self.stop()
+            # Either another thread call thread/pool.stop()
+            # Otherwise it will wait indefinitely
+            self.join()
+            return
+
+        # When ThreadBase has an input queue and previous pools are stopped
+        # We can stop when the input queue is empty
         long_sleep = 0.05
         sleep_time = long_sleep
         while not self.exit_event.is_set():
@@ -236,9 +250,10 @@ class Pool(object):
             self.threads.append(th)
             th.start()
 
-    def stop_when_empty(self):
+    def wait_until_nothing_to_process(self):
+        # Must be called externally (from another thread)
         for th in self.threads:
-            th.stop_when_empty()
+            th.wait_until_nothing_to_process()
 
     def stop(self):
         for th in self.threads:
@@ -331,7 +346,9 @@ class MainLoop(object):
         gevent.signal(gevent.signal.SIGTERM, self.stop)
 
         for pool in self.pools:
-            pool.stop_when_empty()
+            # Either pools stop by themself
+            # Or they will get stopped when input queue is empty
+            pool.wait_until_nothing_to_process()
 
         if not self.exit_event.is_set():
             gevent.signal(gevent.signal.SIGINT, lambda: signal.SIG_IGN)
