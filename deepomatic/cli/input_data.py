87fae2dfa467bba5a823cd26324bb8b0b7884215
Alexandre Moreau <alexandre@deepomatic.com>
2020-03-04 09:42:36 +0100

    speed up exponential average warmup by initializing to estimated fps

--- deepomatic/cli/input_data.py
+++ deepomatic/cli/input_data.py
@@ -350,7 +350,7 @@ class StreamInputData(VideoInputData):
         if last is not None:
             dt = now - last
             self._last_read = now
-            alpha = 0.95
+            alpha = 0 if self._video_fps == 0 else 0.95
             self._video_fps = (1 - alpha) / dt + alpha * self._video_fps
             LOGGER.debug('measured fps: %f' % self._video_fps)
 
