903c2c40aa7a33e80a5c3d65b4d480b2aa726b46
alemoreau <zzozolz@gmail.com>
2020-03-30 15:29:22 +0200

    lint

--- deepomatic/cli/__init__.py
+++ deepomatic/cli/__init__.py
@@ -1,9 +1,10 @@
 import os
 from gevent.monkey import patch_all
+from .version import __version__
+
 
 if os.getenv('DEEPOMATIC_CLI_GEVENT_MONKEY_PATCH', '1') == '1':
     patch_all(thread=False, time=False)  # NOQA
 
-from .version import __version__
 
 __all__ = ["__version__"]
